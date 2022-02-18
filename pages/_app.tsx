4ffc46e35b8dffcbbbc42ff993bf52705c6a8cc2
Marcin Nowakowski <1999nowakowskimarcin@gmail.com>
2022-02-18 13:22:14 +0100

    feat: add input component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,12 @@
+import Input from "../components/Input";
+import "../styles/globals.css";
+
 function MyApp() {
-  return  <h1>Next</h1>
+  return (
+    <div>
+      <Input label="Surname" type="text"/>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
