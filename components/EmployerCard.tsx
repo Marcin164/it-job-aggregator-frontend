4636f14ae0a714ac34d45b3ed06d7183470525be
Marcin Nowakowski <1999nowakowskimarcin@gmail.com>
2022-03-04 14:29:19 +0100

    feat: add employers page

---
+++ components/EmployerCard.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+type Props = {};
+
+const EmployerCard = (props: Props) => {
+  return (
+    <div className="w-[180px] h-[260px] bg-gray-200 rounded-xl my-4">
+      <img src="" alt="" className="w-full h-[180px] overflow-hidden" />
+      <div className="w-full h-[80px] flex justify-center items-center overflow-hidden">
+        <h1 className="text-3xl text-gray-500 font-bold">Frima XYZ</h1>
+      </div>
+    </div>
+  );
+};
+
+export default EmployerCard;
--- pages/dashboard/employers.tsx
+++ pages/dashboard/employers.tsx
@@ -1,18 +1,23 @@
 import React, { ReactElement } from "react";
+import EmployerCard from "../../components/EmployerCard";
+import Pagination from "../../components/Pagination";
 import DashboardLayout from "../../layouts/DashboardLayout";
 
 type Props = {};
 
 const Employers = (props: Props) => {
-  return <h1>Employers</h1>
+  return (
+    <>
+      <Pagination />
+      <div className="grid grid-cols-2 justify-items-center">
+        <EmployerCard />
+      </div>
+    </>
+  );
 };
 
-Employers.getLayout = function getLayout(page:ReactElement) {
-  return (
-    <DashboardLayout>
-        {page}
-    </DashboardLayout>
-  )
-}
+Employers.getLayout = function getLayout(page: ReactElement) {
+  return <DashboardLayout>{page}</DashboardLayout>;
+};
 
 export default Employers;
